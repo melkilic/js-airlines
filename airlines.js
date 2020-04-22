44bbc53da9d072e83e8d4c469000fff19acc7044
joshua-miles <joshua@christetulit.org>
2020-04-22 11:41:13 -0500

    -

--- airlines.js
+++ airlines.js
@@ -1,5 +1,5 @@
 let airlines = [
-    { name: 'United' },
-    { name: 'Spirit' },
-    { name: 'Southwest' },
+    { name: 'United', logoUrl: 'https://media.glassdoor.com/sqll/683/united-airlines-squarelogo-1562605013842.png' },
+    { name: 'Spirit', logoUrl: 'https://freebiemom.r.worldssl.net/wp-content/uploads/2019/08/Spirit-Logo.png' },
+    { name: 'Southwest', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Southwest_Airlines_logo_2014.svg/500px-Southwest_Airlines_logo_2014.svg.png' },
 ]
